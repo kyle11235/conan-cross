3b45ffc818e3bb4ba00b8eea2e8bbadd1427addf
kyle11235 <you@example.com>
2021-05-10 14:55:44 +0800

    clear

---
+++ ndk/test_package/build/14e0cdfaca04e2acc0576ed87609a7a5c25d6ca6/CMakeFiles/example.dir/example.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/example.dir/example.cpp.o: \
+ /root/kyle/conan-cross/ndk/test_package/example.cpp \
+ /usr/include/stdc-predef.h \
+ /root/.conan/data/ndk/0.1/cross/testing/package/e5ac722d270cf7c45ba6c1301f2e878770b1eea3/include/hello.h
---
+++ ndk/test_package/build/1787df5e76ec0694258b4c3db4d98761853885ec/CMakeFiles/example.dir/example.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/example.dir/example.cpp.o: \
+  /root/kyle/conan-cross/ndk/test_package/example.cpp \
+  /root/.conan/data/ndk/0.1/cross/testing/package/2fdea6ea2b74245960b940e1b1e3727157adc92c/include/hello.h
