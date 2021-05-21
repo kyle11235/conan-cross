05e35ffbb14be377fee21d9a013dac40288e4270
someone <someone@example.com>
2021-05-21 21:30:50 +0800

    c

--- linaro_aarch64/build_only.sh
+++ linaro_aarch64/build_only.sh
@@ -3,4 +3,4 @@
 # conan new linaro_aarch64/0.1 -t
 
 # build
-conan create . cross/testing -pr:h ./armv7_cortex_a9_gcc_release -pr:b default
+conan create . cross/testing -pr:h ./linaro_aarch64_gcc_release -pr:b default
