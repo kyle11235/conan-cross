24bd2552e9117dbef93704021b15a73c81cc604e
someone <someone@example.com>
2021-05-21 22:01:39 +0800

    c

--- linaro_aarch64/build.sh
+++ linaro_aarch64/build.sh
@@ -11,4 +11,4 @@ conan upload linaro_aarch64/0.1@cross/testing --all -r=demo-conan-local
 # test resolve
 conan remove "linaro_aarch64*"
 conan install linaro_aarch64/0.1@cross/testing --build=never -r=demo-conan-virtual -pr ./linaro_aarch64_gcc_release
-conan test test_package linaro_aarch64_gcc_release/0.1@cross/testing -pr ./linaro_aarch64_gcc_release
+conan test test_package linaro_aarch64/0.1@cross/testing -pr ./linaro_aarch64_gcc_release
